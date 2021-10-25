1f4c068792344fb83c8da2a7084ee571f3a8546b
karenmrdn <karenmrdn47@gmail.com>
2021-10-25 17:51:22 +0300

    Class validation added with class-validator library

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import { Product } from "./product.model";
 import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
 console.log(_.shuffle([1, 2, 3]));
 
@@ -20,3 +21,13 @@ const loadedProducts = plainToClass(Product, products);
 for (const prod of loadedProducts) {
   console.log(prod.getInfo());
 }
+
+const newProd = new Product("", -4);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.error("Validation error!");
+    console.error(errors);
+  } else {
+    console.log(newProd);
+  }
+});
